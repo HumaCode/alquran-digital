09c382bf9f908f4ebda78d97d17c27ae88b9d5f7
HumaCode <humaidizakaria1998@gmail.com>
2026-05-29 11:02:48 +0700

    refactor: update TafsirSurah model structure with JSON serialization and nullable previous/next surahs

--- lib/app/data/models/tafsir_model.dart
+++ lib/app/data/models/tafsir_model.dart
@@ -1,7 +1,7 @@
 class TafsirSurah {
   int code;
   String message;
-  TafsirData data;
+  Data data;
 
   TafsirSurah({required this.code, required this.message, required this.data});
 
@@ -9,7 +9,7 @@ class TafsirSurah {
     return TafsirSurah(
       code: json['code'] as int,
       message: json['message'] as String,
-      data: TafsirData.fromJson(json['data'] as Map<String, dynamic>),
+      data: Data.fromJson(json['data'] as Map<String, dynamic>),
     );
   }
 
@@ -22,7 +22,7 @@ class TafsirSurah {
   }
 }
 
-class TafsirData {
+class Data {
   int nomor;
   String nama;
   String namaLatin;
@@ -30,9 +30,12 @@ class TafsirData {
   String tempatTurun;
   String arti;
   String deskripsi;
-  List<TafsirAyat> tafsir;
+  Map<String, String> audioFull;
+  List<Tafsir> tafsir;
+  SuratSenya? suratSelanjutnya;
+  SuratSenya? suratSebelumnya;
 
-  TafsirData({
+  Data({
     required this.nomor,
     required this.nama,
     required this.namaLatin,
@@ -40,11 +43,14 @@ class TafsirData {
     required this.tempatTurun,
     required this.arti,
     required this.deskripsi,
+    required this.audioFull,
     required this.tafsir,
+    required this.suratSelanjutnya,
+    required this.suratSebelumnya,
   });
 
-  factory TafsirData.fromJson(Map<String, dynamic> json) {
-    return TafsirData(
+  factory Data.fromJson(Map<String, dynamic> json) {
+    return Data(
       nomor: json['nomor'] as int,
       nama: json['nama'] as String,
       namaLatin: json['namaLatin'] as String,
@@ -52,9 +58,16 @@ class TafsirData {
       tempatTurun: json['tempatTurun'] as String,
       arti: json['arti'] as String,
       deskripsi: json['deskripsi'] as String,
+      audioFull: Map<String, String>.from(json['audioFull'] as Map),
       tafsir: (json['tafsir'] as List<dynamic>)
-          .map((e) => TafsirAyat.fromJson(e as Map<String, dynamic>))
+          .map((e) => Tafsir.fromJson(e as Map<String, dynamic>))
           .toList(),
+      suratSelanjutnya: json['suratSelanjutnya'] is Map<String, dynamic>
+          ? SuratSenya.fromJson(json['suratSelanjutnya'] as Map<String, dynamic>)
+          : null,
+      suratSebelumnya: json['suratSebelumnya'] is Map<String, dynamic>
+          ? SuratSenya.fromJson(json['suratSebelumnya'] as Map<String, dynamic>)
+          : null,
     );
   }
 
@@ -67,19 +80,54 @@ class TafsirData {
       'tempatTurun': tempatTurun,
       'arti': arti,
       'deskripsi': deskripsi,
+      'audioFull': audioFull,
       'tafsir': tafsir.map((e) => e.toJson()).toList(),
+      'suratSelanjutnya': suratSelanjutnya?.toJson(),
+      'suratSebelumnya': suratSebelumnya?.toJson(),
     };
   }
 }
 
-class TafsirAyat {
+class SuratSenya {
+  int nomor;
+  String nama;
+  String namaLatin;
+  int jumlahAyat;
+
+  SuratSenya({
+    required this.nomor,
+    required this.nama,
+    required this.namaLatin,
+    required this.jumlahAyat,
+  });
+
+  factory SuratSenya.fromJson(Map<String, dynamic> json) {
+    return SuratSenya(
+      nomor: json['nomor'] as int,
+      nama: json['nama'] as String,
+      namaLatin: json['namaLatin'] as String,
+      jumlahAyat: json['jumlahAyat'] as int,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'nomor': nomor,
+      'nama': nama,
+      'namaLatin': namaLatin,
+      'jumlahAyat': jumlahAyat,
+    };
+  }
+}
+
+class Tafsir {
   int ayat;
   String teks;
 
-  TafsirAyat({required this.ayat, required this.teks});
+  Tafsir({required this.ayat, required this.teks});
 
-  factory TafsirAyat.fromJson(Map<String, dynamic> json) {
-    return TafsirAyat(
+  factory Tafsir.fromJson(Map<String, dynamic> json) {
+    return Tafsir(
       ayat: json['ayat'] as int,
       teks: json['teks'] as String,
     );

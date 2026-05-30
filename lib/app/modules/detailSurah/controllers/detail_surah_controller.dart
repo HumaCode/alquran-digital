d73a05f5a441cead325829558dfc3c13cbd07599
HumaCode <humaidizakaria1998@gmail.com>
2026-05-30 22:29:09 +0700

    fix: ganti import widgets.dart menjadi material.dart untuk atasi referensi kelas Colors yang tidak terdefinisi

--- lib/app/modules/detailSurah/controllers/detail_surah_controller.dart
+++ lib/app/modules/detailSurah/controllers/detail_surah_controller.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:shared_preferences/shared_preferences.dart';

d1afa2faaebf773b34da3ec5e935d3bd8e8de37c
HumaCode <humaidizakaria1998@gmail.com>
2026-06-02 09:42:15 +0700

    feat: register widget assets in pubspec and constants, and set preview images and icons for Android Home Widgets

--- lib/app/constants/R/asset_images.dart
+++ lib/app/constants/R/asset_images.dart
@@ -1,3 +1,7 @@
 class AssetsImage {
   const AssetsImage();
+
+  final String ayatHarian = 'assets/image/ayat_harian.png';
+  final String jdwlSholat = 'assets/image/jdwl_sholat.png';
+  final String tilawah = 'assets/image/tilawah.png';
 }
